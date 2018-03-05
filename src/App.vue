b56515409ccca4a1b7983253f1b8f8c38f542e27
Fabien Sanchez <fabien.sanchez@campus-numerique-in-the-alps.com>
2018-03-05 14:41:40 +0100

    etape 3

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,98 @@
 <template>
-  <div id="app">
-    <img src="https://cdn.pixabay.com/photo/2014/11/27/12/24/coffee-547490_960_720.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vueify" target="_blank">vueify</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div id="app">
+        <img src="/src/assets/coffe-cup.jpg">
+        <h1>{{ msg }}</h1>
+        <h2>que faire ?</h2>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="nav nav-tabs nav-fill">
+                        <li class="nav-item">
+                            <a class="nav-link active" @click="onListClick">Liste</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" @click="onMapClick">Carte</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <List></List>
+                    <Map></Map>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    import List from './components/MachinesList.vue'
+    import Map from './components/MachinesMap.vue'
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+                msg: 'Coffee Machine Vader'
+            }
+        },
+        methods: {
+            onListClick: function () {
+                alert('Vous avez cliqué sur liste')
+            },
+            onMapClick: function () {
+                alert('Vous avez cliqué sur map');
+            }
+        },
+        components: {
+            List,
+            Map
+        }
     }
-  }
-}
 </script>
 
 <style scoped>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 
-img {
-  width: 200px;
-  height: 200px;
-}
+    img {
+        width: 200px;
+        height: 200px;
+    }
 
-h1, h2 {
-  font-weight: normal;
-}
+    h1, h2 {
+        font-weight: normal;
+        margin-bottom: 15px;
+        margin-top: 15px;
+    }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+    h1 {
+        color: #53240f;
+    }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+    h2 {
+        color: chocolate;
+    }
 
-a {
-  color: #42b983;
-}
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
 </style>
---
+++ src/components/MachinesMap.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>map</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "machines-map.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
